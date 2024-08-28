7021bd9fcb8b01e721d7989edfd97b550a47c20a
alexandrawilkowski <alexandrawilkowski@alexandrasair6.attlocal.net>
2024-08-28 15:05:41 -0500

    Sending message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Hi groupmates!");
+		System.out.println("Hi groupmates!");//computer1
+		System.out.println("Hello");//computer2
 	}
 }
